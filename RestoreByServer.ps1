d3464e6dd0d27c3bfca706b3fb4948c7103a4176
Christian Stuart Grimberg <christian.grimberg@outlook.com>
2018-02-22 10:50:44 -0300

    Select one Server of Backup Vault

--- RestoreByServer.ps1
+++ RestoreByServer.ps1
@@ -1,9 +1,13 @@
 # Connect to Azure and select the correct 
 #.\ConnectAndSelectSuscription.ps1
 
-# Use your Vault by Name
+# Declare message imput to users
 $MessageVault = "Input Name of Service Vault: "
+$MessagePassword = "Imput Password of "
+$MessageServer = "Input Name of Server: "
 $MessageThanks = "Thank You!"
+$DestinationRestoredData = "D:\RestoredData\"
+$NumberFolder = 0
 
 # Select the Service Vault by user
 $MessageVault
@@ -11,3 +15,11 @@ $VaultName = Read-Host
 $MessageThanks
 $Vault = Get-AzureRmRecoveryServicesVault -Name $VaultName
 Set-AzureRmRecoveryServicesVaultContext -Vault $Vault
+
+# Select the Server
+# Download the credential vault to C:\Vaults and named as credentials.VaultCredentials
+$MessageServer
+$Server = Read-Host
+$MessageThanks
+$FileVault = "C:\Vaults\credentials.VaultCredentials"
+$BackupServer = Get-OBAlternateBackupServer -VaultCredentials $FileVault -Name $Server
